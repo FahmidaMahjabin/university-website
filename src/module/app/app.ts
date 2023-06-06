a474145bb50b9ae98fe189d91cee31f13128f7be
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2023-06-05 22:55:47 -0500

    test

--- src/module/app/app.ts
+++ src/module/app/app.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express, { Request } from 'express'
 import cors from 'cors'
 const app = express()
 app.use(cors())
 // body perser
 app.use(express.json())
 app.use(express.urlencoded())
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
